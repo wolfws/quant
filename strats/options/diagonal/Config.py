9274246d69c481ab87a2eb832132279a1e0340e0
wolfws <wolfws@users.noreply.github.com>
2018-02-15 00:57:23 -0500

    Delete Config.py

--- strats/options/diagonal/Config.py
+++
@@ -1,5 +0,0 @@
-MONGOHOST = "localhost"
-MONGODATABASE = "options"
-MAXIMUM_NUMBER_OF_PERMUTATIONS = 300000 # limit maximum number of permutations / strategy scenarios
-VERBOSE = True
-UNDERLYING_TS_DAYS_BACK = 500
